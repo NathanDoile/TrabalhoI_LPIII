f343a12bce9050c9e2f17027c31f384a3465a5bd
Nathan Doile <100170461+NathanDoile@users.noreply.github.com>
2023-06-26 11:04:15 -0300

    Update NomePokemon.java

--- TrabalhoI_LPIII/src/main/java/br/com/ifsul/domain/enums/NomePokemon.java
+++ TrabalhoI_LPIII/src/main/java/br/com/ifsul/domain/enums/NomePokemon.java
@@ -3,7 +3,7 @@ package br.com.ifsul.domain.enums;
 public enum NomePokemon {
 	SQUIRTLE(1,0), WARTOTLE(16,160), BLASTOISE(36,360),
 	CHARMANDER(1,0), CHARMELEON(16,160), CHARIZARD(36,360),
-	BULBASAUR(16,0), IVYSAUR(16,160), VENUSAUR(32,320);
+	BULBASAUR(1,0), IVYSAUR(16,160), VENUSAUR(32,320);
 
 	private int nivel;
 
